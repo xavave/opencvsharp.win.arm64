2bbd6691b1fb6397e58718716b5fc78e37f70a73
Xavier Averbouch <xavieraverbouch@gmail.com>
2025-10-21 15:26:55 +0200

    Update download links for OpenCV files

--- download_opencv_windows.ps1
+++ download_opencv_windows.ps1
@@ -1,7 +1,8 @@
 $tag = "4.11.0.20250505"
+$version = "4100"
 $uriArray = @(
-    "https://github.com/shimat/opencv_files/releases/download/${tag}/opencv_win_x64.zip"
-    "https://github.com/shimat/opencv_files/releases/download/${tag}/opencv_win_x86.zip"
+    "https://github.com/shimat/opencv_files/releases/download/${tag}/opencv${version}_win_x64.zip"
+    "https://github.com/shimat/opencv_files/releases/download/${tag}/opencv${version}_win_x86.zip"
 )
 
 function Download($uri, $outFile) {
